a4856a4705377ada7f86905283501ad8811e12b7
Michael Alex Martinez <mike.alex.martinez@gmail.com>
2019-03-23 13:53:10 +0000

    fixed dev cors issue

--- src/index.js
+++ src/index.js
@@ -11,8 +11,13 @@ server.start(
   {
     cors: {
       credentials: true,
-      origin: process.env.FRONTEND_URL
-    }
+      origin: [
+        process.env.FRONTEND_URL,
+        process.env.YOGA_PORT,
+        process.env.PLAYGROUND
+      ]
+    },
+    port: process.env.YOGA_PORT
   },
   deets => {
     console.log(`Server is now running on port http://localhost:${deets.port}`);
